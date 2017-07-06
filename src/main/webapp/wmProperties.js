aa59a59d7b417dc433bb1681b9a2c3f7970090f5
root <root@61949b3d2f95>
2017-07-06 13:22:09 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_eClpDEcrwd",
+  "homePage" : "Main",
+  "name" : "Auto_eClpDEcrwd",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
